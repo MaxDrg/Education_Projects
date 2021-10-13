2805c21317793e085a1076d89a5699d30024facc
MaximDergachov <maximadda2001@gmail.com>
2021-10-13 17:48:22 +0300

    3.0

---
+++ system_analysis_and_design/lab3/WindowsFormsApplication3/ImageTransformationsForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageTransformationsApp
+{
+    public partial class ImageTransformationsForm : Form
+    {
+        private string _file;
+
+        public ImageTransformationsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            string file = OpenFile();
+            if (file != null)
+            {
+                this.FileTextBox.Text = file;
+                _file = file;
+            }
+        }
+
+        public static string OpenFile()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+                return dlg.FileName;
+
+            return null;
+        }
+
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            if (_file == null)
+                return;
+
+        DisposePreviousImage();
+
+            RotationImageTransformation rotation = new RotationImageTransformation((double)this.AngleNumericUpDown.Value);
+            StretchImageTransformation stretch = new StretchImageTransformation(
+                (double)this.HorizStretchNumericUpDown.Value / 100,
+                (double)this.VertStretchNumericUpDown.Value / 100);
+            FlipImageTransformation flip = new FlipImageTransformation(this.FlipHorizontalCheckBox.Checked, this.FlipVerticalCheckBox.Checked);
+
+            DensityImageTransformation density = new DensityImageTransformation(
+                (double)this.AlphaNumericUpDown.Value / 100,
+                (double)this.RedNumericUpDown.Value / 100,
+                (double)this.GreenNumericUpDown.Value / 100,
+                (double)this.BlueNumericUpDown.Value / 100
+              );
+            var bmp = ImageTransformer.Apply(_file,
+              new IImageTransformation[] { rotation, stretch, flip, density });
+
+            this.ImagePictureBox.Image = bmp;
+        }
+
+
+
+        private void DisposePreviousImage()
+        {
+            if (this.ImagePictureBox.Image != null)
+            {
+                var tmpImg = this.ImagePictureBox.Image;
+                this.ImagePictureBox.Image = null;
+                tmpImg.Dispose();
+            }
+        }
+    }
+}
---
+++ system_analysis_and_design/lab3/WindowsFormsApplication3/Matrices.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageTransformationsApp
+{
+    class Matrices
+    {
+        public static double[,] CreateIdentityMatrix(int length)
+        {
+            double[,] matrix = new double[length, length];
+
+            for (int i = 0, j = 0; i < length; i++, j++)
+                matrix[i, j] = 1;
+
+            return matrix;
+        }
+        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+        {
+            var matrix1Rows = matrix1.GetLength(0);
+            var matrix1Cols = matrix1.GetLength(1);
+            var matrix2Rows = matrix2.GetLength(0);
+            var matrix2Cols = matrix2.GetLength(1);
+
+            if (matrix1Cols != matrix2Rows)
+                throw new InvalidOperationException
+                  ("User error");
+
+            double[,] product = new double[matrix1Rows, matrix2Cols];
+
+            for (int matrix1_row = 0; matrix1_row < matrix1Rows; matrix1_row++) {
+
+                for (int matrix2_col = 0; matrix2_col < matrix2Cols; matrix2_col++) {
+
+                    for (int matrix1_col = 0; matrix1_col < matrix1Cols; matrix1_col++) {
+                        product[matrix1_row, matrix2_col] +=
+                          matrix1[matrix1_row, matrix1_col] *
+                          matrix2[matrix1_col, matrix2_col];
+                    }
+                }
+            }
+
+            return product;
+        }
+    }
+}
